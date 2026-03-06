4fd169c4fdb7ebf3f0c157c2c5e2e694399b039d
Ishita282 <littleminiishita@gmail.com>
2026-03-06 19:08:56 +0530

    update api_uri

--- backend-todo/index.js
+++ backend-todo/index.js
@@ -11,9 +11,7 @@ dbConnection();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "https://your-frontend.vercel.app",
-}));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json({
