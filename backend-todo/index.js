18b406158ee717c4d1ad14f44174e1ae2ba9f2f6
Ishita282 <littleminiishita@gmail.com>
2026-03-06 19:24:42 +0530

    update port

--- backend-todo/index.js
+++ backend-todo/index.js
@@ -23,5 +23,5 @@ app.use("/tasks", taskroute);
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
