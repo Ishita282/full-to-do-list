2578dd8bfc63ec02039318de22072a4f8906efae
Ishita282 <littleminiishita@gmail.com>
2026-03-06 17:25:20 +0530

    update api_uri

--- frontend-todo/src/service/taskService.js
+++ frontend-todo/src/service/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://full-to-do-list.onrender.com';
+const API_URL = 'https://full-to-do-list.onrender.com/tasks';
 
 // Get all tasks
 export const getAllTask = () => {
