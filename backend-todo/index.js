bbecfe0ac72c1c85c1608fa14b1cd8ffe00aab09
Ishita282 <littleminiishita@gmail.com>
2026-03-06 20:14:08 +0530

    update error handler

--- backend-todo/index.js
+++ backend-todo/index.js
@@ -13,6 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome...",
