a512450b8760a8f23a769540f2697ca721f73b59
Ishita282 <littleminiishita@gmail.com>
2026-03-09 01:13:28 +0530

    db update

--- backend-todo/config/db.js
+++ backend-todo/config/db.js
@@ -4,11 +4,20 @@ require("dotenv").config();
 function dbConnection() {
   const DB_URL = process.env.MONGO_URI;
 
-  mongoose.connect(DB_URL );
+  console.log('Testing MongoDB connection...');
+
+  mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000,
+  });
 
   const db = mongoose.connection;
 
-   db.on("error", console.error.bind(console, "Connection Error"));
+  db.on("error", (err) => {
+    console.error("MongoDB Connection Error:", err);
+  });
+
   db.once("open", function () {
     console.log("Database is connected successfully!");
   });
